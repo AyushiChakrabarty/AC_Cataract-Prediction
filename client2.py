d148d4bf0e8a3d906721ca15bf0b8f1013e01a02
Ayushi Chakrabarty <67193440+AyushiChakrabarty@users.noreply.github.com>
2021-10-14 16:51:50 +0530

    Add files via upload

---
+++ client2.py
@@ -0,0 +1,155 @@
+import flwr as fl
+import tensorflow as tf
+from tensorflow import keras
+import sys
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np 
+import pandas as pd 
+import cv2
+import random
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+#Load the dataset (x and y)
+x = np.load('x.npy')
+y = np.load('y.npy')
+
+#Train, test and split
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2)
+
+image_size=224
+
+#Define VGG19
+from tensorflow.keras.applications.vgg19 import VGG19
+vgg = VGG19(weights="imagenet",include_top = False,input_shape=(image_size,image_size,3))
+
+
+for layer in vgg.layers:
+    layer.trainable = False
+
+# Auxillary methods
+''''
+def getDist(y):
+    ax = sns.countplot(y)
+    ax.set(title="Count of data classes")
+    plt.show()
+
+def getData(dist, x, y):
+    dx = []
+    dy = []
+    counts = [0 for i in range(10)]
+    for i in range(len(x)):
+        if counts[y[i]]<dist[y[i]]:
+            dx.append(x[i])
+            dy.append(y[i])
+            counts[y[i]] += 1
+        
+    return np.array(dx), np.array(dy)
+'''
+#Defining the model
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Flatten,Dense
+
+#Call model
+model = Sequential()
+model.add(vgg)
+model.add(Flatten())
+model.add(Dense(1,activation="sigmoid"))
+
+model.compile(optimizer="adam",loss="binary_crossentropy",metrics=["accuracy"])
+
+
+from tensorflow.keras.callbacks import ModelCheckpoint,EarlyStopping
+checkpoint = ModelCheckpoint("vgg19.h5",monitor="val_acc",verbose=1,save_best_only=True,
+                             save_weights_only=False,period=1)
+earlystop = EarlyStopping(monitor="val_acc",patience=5,verbose=1)
+
+#Model fitting
+history = model.fit(x_train,y_train,batch_size=32,epochs=8,validation_data=(x_test,y_test),
+                    verbose=1,callbacks=[checkpoint,earlystop])
+
+#To evaluate the loss and accuracy values
+loss,accuracy = model.evaluate(x_test,y_test)
+print("loss:",loss)
+print("Accuracy:",accuracy)
+
+from sklearn.metrics import confusion_matrix,classification_report,accuracy_score
+y_pred = model.predict_classes(x_test)
+#Accuracy
+accuracy_score(y_test,y_pred)
+
+#To print the classification report
+print(classification_report(y_test,y_pred))
+
+#To plot the model accuracy and model loss after each epoch
+plt.style.use("ggplot")
+fig = plt.figure(figsize=(12,6))
+epochs = range(1,9)
+plt.subplot(1,2,1)
+plt.plot(epochs,history.history["accuracy"],"go-")
+plt.plot(epochs,history.history["val_accuracy"],"ro-")
+plt.title("Model Accuracy")
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend(["Train","val"],loc = "upper left")
+
+plt.subplot(1,2,2)
+plt.plot(epochs,history.history["loss"],"go-")
+plt.plot(epochs,history.history["val_loss"],"ro-")
+plt.title("Model Loss")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend(["Train","val"],loc = "upper left")
+plt.show()
+
+#Actual vs Prediction
+plt.figure(figsize=(12,7))
+for i in range(10):
+    sample = random.choice(range(len(x_test)))
+    image = x_test[sample]
+    category = y_test[sample]
+    pred_category = y_pred[sample]
+    
+    if category== 0:
+        label = "Normal"
+    else:
+        label = "Cataract"
+        
+    if pred_category== 0:
+        pred_label = "Normal"
+    else:
+        pred_label = "Cataract"
+        
+    plt.subplot(2,5,i+1)
+    plt.imshow(image)
+    plt.xlabel("Actual:{}\nPrediction:{}".format(label,pred_label))
+plt.tight_layout()
+
+
+# Define Flower client
+class FlowerClient(fl.client.NumPyClient):
+    def get_parameters(self):
+        return model.get_weights()
+
+    def fit(self, parameters, config):
+        model.set_weights(parameters)
+        r = model.fit(x_train, y_train, epochs=1, validation_data=(x_test, y_test), verbose=0)
+        hist = r.history
+        print("Fit history : " ,hist)
+        return model.get_weights(), len(x_train), {}
+
+    def evaluate(self, parameters, config):
+        model.set_weights(parameters)
+        loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
+        print("Eval accuracy : ", accuracy)
+        return loss, len(x_test), {"accuracy": accuracy}
+
+# Start Flower client
+fl.client.start_numpy_client(
+        server_address="localhost:"+str(sys.argv[1]), 
+        client=FlowerClient(), 
+        grpc_max_message_length = 1024*1024*1024
+)
